264fc3789b9bcfc311f9286a2753e43409f06315
varthikavarthika <119730787+varthikavarthika@users.noreply.github.com>
2023-01-04 20:37:46 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a%400==0||a%100!=0&&a%4==0)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Program_to_find_out_the_entered_year_is_leap_year_or_not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%400==0||a%100!=0&&a%4==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
