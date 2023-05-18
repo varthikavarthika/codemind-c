2e8e20ce940c1df61eba1f4a370b98aa55118055
varthikavarthika <119730787+varthikavarthika@users.noreply.github.com>
2023-05-18 22:21:52 +0530

    #include<stdio.h> int main()
    {
        int a,b,i=0;
        scanf("%d %d",&a,&b);
        for(i=1;i<=a*b;i++)
        {
            if(i%a==0&&i%b==0)
            {
                printf("%d",i);
                break;
            }
        }
    }

---
+++ LCM.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+
+{
+    int a,b,i=0;
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=a*b;i++)
+    {
+        if(i%a==0&&i%b==0)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
