e4c386b3a8315bc523c5ff274558ab37b6e22586
varthikavarthika <119730787+varthikavarthika@users.noreply.github.com>
2023-05-20 06:06:37 +0530

    #include<stdio.h> int main() {     int n,i,a=0,m=1;     scanf("%d",&n);     for(i=1;n>0;i++)     {         i=n%10;         n=n/10;         a=a+i;         m=m*i;     }     if(a==m)     {         printf("Spy Number");     }     else     {         printf("Not Spy Number");     } }

---
+++ Spy_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a=0,m=1;
+    scanf("%d",&n); 
+    for(i=1;n>0;i++)
+    {
+        i=n%10;
+        n=n/10;
+        a=a+i;
+        m=m*i;
+    }
+    if(a==m)
+    {
+        printf("Spy Number");
+    }
+    else
+    {
+        printf("Not Spy Number");
+    }
+}
